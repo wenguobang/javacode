d2ee665751f3f65ada268d19499187f76ff245d1
wenguobang <15191902455@163.com>
2018-03-24 15:29:01 +0800

    second

--- Java_Test/src/com/wgb/java/HelloWorld.java
+++ Java_Test/src/com/wgb/java/HelloWorld.java
@@ -3,6 +3,18 @@ package com.wgb.java;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello world!");
+		Test t = new Test();
+		t.Set(3);
+		System.out.println(t.Get());
 	}
 
+}
+class Test{
+	private int i;
+	public void Set(int i) {
+		this.i = i;
+	}	
+	public int Get() {
+		return i;
+	}
 }
